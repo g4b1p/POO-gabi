005b19f7cc3949f2d8d3f361a21c8ac3b1ee6861
gabi <plataramires17@gmail.com>
2024-09-12 17:51:05 -0300

    ee

---
+++ reprodMP3/reprodMP3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace reprodMP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "|◀";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(497, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "▶";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(556, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "▶|";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(318, 353);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(413, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(35, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 290);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(327, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "agregar cancion";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ reprodMP3/reprodMP3/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reprodMP3
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("winmm.dll")]
+        static extern Int32 mciSendString(string command, StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
+
+        //public const int MM_MCINOTIFY = 953;
+
+        //string strFilePath = @"C:\Users\A-01\Desktop\Pensamientos.mp3";
+
+        string[] canciones = new string[]
+        {
+            @"C:\Users\A-01\Downloads\AIRBAG - Intoxicarme.mp3",
+            @"C:\Users\A-01\Downloads\AIRBAG - Apocalipsis Confort.mp3",
+            @"C:\Users\A-01\Downloads\AIRBAG - Como un Diamante.mp3",
+            @"C:\Users\A-01\Downloads\AIRBAG - Über Puber.mp3"
+        };
+
+        string[] nombres = new string[]
+        {
+            "Intoxicarme",
+            "Apocalipsis Confort",
+            "Como un Diamante",
+            "Über Puber"
+        };
+
+        int indCancionActual = 0;
+
+        int duracionCancion = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.Items.AddRange(nombres);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //mciSendString("seek MediaFile to start", null, 0, IntPtr.Zero);
+            // comienzo de 0
+
+            mciSendString("stop MediaFile", null, 0, IntPtr.Zero);
+            mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+
+            indCancionActual--;
+
+            if (indCancionActual < 0)
+            {
+                indCancionActual = canciones.Length - 1;
+            }
+
+            string rutaC = canciones[indCancionActual];
+
+            mciSendString($"open \"{rutaC}\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+
+            const int MAX_PATH = 255;
+
+            string sAlias = "MediaFile";
+
+            StringBuilder sbBuffer = new StringBuilder(MAX_PATH);
+            mciSendString("set " + sAlias + " time format milliseconds", null, 0, IntPtr.Zero);
+            // Obtenemos el largo del archivo, en millisegundos.
+
+            mciSendString("status " + sAlias + " length", sbBuffer, MAX_PATH, IntPtr.Zero);
+
+            duracionCancion = int.Parse(sbBuffer.ToString());
+
+            trackBar1.Maximum = duracionCancion;
+
+            textBox1.Text = nombres[indCancionActual];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "| |")
+            {
+                mciSendString("stop MediaFile", null, 0, IntPtr.Zero);
+                button2.Text = "▶️";
+                
+            }
+            else
+            {
+                string rutaC = canciones[indCancionActual];
+
+                mciSendString($"open \"{rutaC}\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+                mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+
+                const int MAX_PATH = 255;
+
+                string sAlias = "MediaFile";
+
+                StringBuilder sbBuffer = new StringBuilder(MAX_PATH);
+                mciSendString("set " + sAlias + " time format milliseconds", null, 0, IntPtr.Zero);
+                // Obtenemos el largo del archivo, en millisegundos.
+
+                mciSendString("status " + sAlias + " length", sbBuffer, MAX_PATH, IntPtr.Zero);
+
+                duracionCancion = int.Parse(sbBuffer.ToString());
+
+                trackBar1.Maximum = duracionCancion;
+
+                textBox1.Text = nombres[indCancionActual];
+
+                button2.Text = "| |";
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mciSendString("stop MediaFile", null, 0, IntPtr.Zero);
+            mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+
+            indCancionActual++;
+
+            if (indCancionActual >= canciones.Length)
+            {
+                indCancionActual = 0;
+            }
+
+            string rutaC = canciones[indCancionActual];
+
+            mciSendString($"open \"{rutaC}\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+
+            const int MAX_PATH = 255;
+
+            string sAlias = "MediaFile";
+
+            StringBuilder sbBuffer = new StringBuilder(MAX_PATH);
+            mciSendString("set " + sAlias + " time format milliseconds", null, 0, IntPtr.Zero);
+            // Obtenemos el largo del archivo, en millisegundos.
+
+            mciSendString("status " + sAlias + " length", sbBuffer, MAX_PATH, IntPtr.Zero);
+
+            duracionCancion = int.Parse(sbBuffer.ToString());
+
+            trackBar1.Maximum = duracionCancion;
+
+            textBox1.Text = nombres[indCancionActual];
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                mciSendString("stop MediaFile", null, 0, IntPtr.Zero);
+                mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+
+                indCancionActual = listBox1.SelectedIndex;
+
+                string rutaC = canciones[indCancionActual];
+
+                mciSendString($"open \"{rutaC}\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+                mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+
+                const int MAX_PATH = 255;
+
+                string sAlias = "MediaFile";
+
+                StringBuilder sbBuffer = new StringBuilder(MAX_PATH);
+                mciSendString("set " + sAlias + " time format milliseconds", null, 0, IntPtr.Zero);
+                // Obtenemos el largo del archivo, en millisegundos.
+
+                mciSendString("status " + sAlias + " length", sbBuffer, MAX_PATH, IntPtr.Zero);
+
+                duracionCancion = int.Parse(sbBuffer.ToString());
+
+                trackBar1.Maximum = duracionCancion;
+
+                textBox1.Text = nombres[indCancionActual];
+
+                button2.Text = "| |";
+
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //int posSeleccionada = trackBar1.Value;
+
+            mciSendString($"seek MediaFile to {trackBar1.Value}", null, 0, IntPtr.Zero);
+
+            button2.Text = "| |";
+
+            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = " ";
+            openFileDialog1.FileName = "cancion.mp3";
+            openFileDialog1.Filter = "MP3 |*.mp3";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Add(Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName));
+            }
+        }
+    }
+}
