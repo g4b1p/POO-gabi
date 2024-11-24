77d89e751f13da6371207c550d31071eaeffbdfe
“gabi” <plataramirez17@gmail.com>
2024-11-24 14:03:30 -0300

    ejer1 arreglado

--- ejercicio-obligatorio_1/Program.cs
+++ ejercicio-obligatorio_1/Program.cs
@@ -8,22 +8,8 @@ namespace ejercicio_obligatorio_1
 {
     class cuenta
     {
+        public string titular { get; set; }
         public double cantidad { get; set; }
-        public string titular {  get; set; }
-        /*public string titular
-        {
-            get
-            {
-                return Titular;
-            }
-            set
-            {
-                if (value != null)
-                {
-                    Titular = value;
-                }
-            }
-        }*/
 
         public cuenta(string titular, double cantidad)
         {
@@ -31,11 +17,9 @@ namespace ejercicio_obligatorio_1
             this.cantidad = cantidad;
         }
 
-        public cuenta (string titular) 
+        public cuenta (string titular)
         {
             this.titular = titular;
-            //titular = "juan";
-            cantidad = 0;
         }
 
         public void ingresar (double dineroIngresado)
@@ -46,7 +30,7 @@ namespace ejercicio_obligatorio_1
             }
             else
             {
-                Console.WriteLine("ERROR, debe ingresar un numero positivo");
+                Console.WriteLine("debe ingresar un numero positivo");
             }
         }
 
@@ -66,45 +50,17 @@ namespace ejercicio_obligatorio_1
     {
         static void Main(string[] args)
         {
-            cuenta cuenta1 = new cuenta("juan", 1000);
+            cuenta cuenta1 = new cuenta("raul", 1000);
             cuenta1.ingresar(500);
             cuenta1.retirar(100);
             Console.WriteLine("titular: " + cuenta1.titular + ", cantidad: " + cuenta1.cantidad);
 
             Console.WriteLine();
 
-            cuenta cuenta2 = new cuenta("maria");
+            cuenta cuenta2 = new cuenta("rocio");
             cuenta2.ingresar(500);
             cuenta2.retirar(200);
             Console.WriteLine("titular: " + cuenta2.titular + ", cantidad: " + cuenta2.cantidad);
-
-            Console.WriteLine();
-
-            /*Console.WriteLine("1. ingresar dinero");
-            Console.WriteLine("2. retirar dinero");
-
-            Console.WriteLine("* seleccione opcion *");
-
-            int opcion;
-
-            opcion = int.Parse(Console.ReadLine());
-
-            switch (opcion)
-            {
-                case 1:
-                    Console.Write("cuanto desea ingresar: ");
-                    double dineroIngresado = double.Parse(Console.ReadLine());
-                    cuenta.ingresar(dineroIngresado);
-                    Console.WriteLine("su saldo es: $" + cuenta.cantidad);
-                    break;
-                case 2:
-                    Console.Write("cuanto desea retirar: ");
-                    double dineroSacado = double.Parse(Console.ReadLine());
-                    cuenta.retirar(dineroSacado);
-                    Console.WriteLine("su saldo es: $" + cuenta.cantidad);
-                    break;
-            }
-            */
 
             Console.ReadKey();
         }
